1a24d24369f7e0f15cecba7ab18e1945305e13d9
Dylan Cutler <DCtheTall@users.noreply.github.com>
2018-02-12 00:28:31 -0500

    view engine

--- app.js
+++ app.js
@@ -13,4 +13,10 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false, limit: '2mb' }));
 app.use(bodyParser.json({ limit: '5mb' }));
 
+// Views
+app.set('view engine', 'pug');
+app.set('views', `${__dirname}/views/`);
+
+app.get('/', (req, res) => res.render('index'));
+
 module.exports = app;
