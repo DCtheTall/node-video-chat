852903fea7409d1f7f3952ac8b13a65d1c746774
Dylan Cutler <DCtheTall@users.noreply.github.com>
2018-03-04 20:00:04 -0500

    fixing signup mutation

--- src/server/schema/mutation/user/SignupUser.js
+++ src/server/schema/mutation/user/SignupUser.js
@@ -31,15 +31,16 @@ export default {
     try {
       let [user, created] = await models.user.findOrInitialize({ // eslint-disable-line prefer-const
         where: {
-          username: { $iLike: username.replace(/\s/i, '') },
-          email: email.trim().toLowerCase(),
-        },
-        defaults: {
-          username,
-          password,
+          $or: [
+            { username: { $iLike: username.trim() } },
+            { email: email.trim().toLowerCase() },
+          ],
         },
       });
       if (!created) return { success: false, message: 'There is already an account with that email/username' };
+      user.email = email;
+      user.username = username.trim();
+      user.password = password;
       user = await user.save();
       const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: 24 * 60 * 60 });
       req.cookies.set(process.env.COOKIE_KEY, token, { signed: true, maxAge: Date.now() + (24 * 60 * 60 * 1e3) });
