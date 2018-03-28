783fd7ec9ad907bf5f9e07b6b63cef8fe0be91f0
Dylan Cutler <DCtheTall@users.noreply.github.com>
2018-03-27 23:05:24 -0400

    create message thread

--- src/client/components/Contacts/ContactList/Contact.jsx
+++ src/client/components/Contacts/ContactList/Contact.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'react-apollo';
 import { compose } from 'redux';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 import QUERY_MESSAGE_THREADS from '../../../graphql/queries/message-threads/message-threads.graphql';
 import CREATE_MESSAGE_THREAD from '../../../graphql/mutations/message-threads/create.graphql';
+import { GET_MESSAGE_THREAD_ROUTE } from '../../../constants';
+import { addError, clearError } from '../../../actions/error';
 import UserInfo from '../../shared/UserInfo';
 import '../../../styles/contact.scss';
 
@@ -25,14 +29,18 @@ class Contact extends React.PureComponent {
   /**
    * @returns {Promise<undefined>} opens existing message thread between users or creates one
    */
-  openTextChat() {
+  async openTextChat() {
+    this.props.clearError();
     const messageThread = this.props.messageThreads.data &&
       this.props.messageThreads.data.find(({ user }) => user.id === this.props.user.id);
-    if (messageThread) return null; // Handle later
-    return this.props.createMessageThread({
+    if (messageThread) return this.props.history.push(GET_MESSAGE_THREAD_ROUTE(messageThread.id));
+    const mutation = await this.props.createMessageThread({
       variables: { contactId: this.props.id },
       refetchQueries: [{ query: QUERY_MESSAGE_THREADS }],
     });
+    const { data: { result } } = mutation;
+    if (!result.success) this.props.addError(result.message);
+    return this.props.history.push(GET_MESSAGE_THREAD_ROUTE(result.threadId));
   }
   /**
    * render
@@ -69,10 +77,15 @@ Contact.propTypes = {
   messageThreads: PropTypes.shape({
     data: PropTypes.arrayOf(PropTypes.shape),
   }),
+  history: PropTypes.shape(),
+  addError: PropTypes.func,
+  clearError: PropTypes.func,
   createMessageThread: PropTypes.func,
 };
 
 export default compose(
+  withRouter,
+  connect(null, { addError, clearError }),
   graphql(
     QUERY_MESSAGE_THREADS,
     { name: 'messageThreads' },
--- src/client/constants/index.js
+++ src/client/constants/index.js
@@ -7,6 +7,9 @@ export const CONTACT_REQUESTS_ROUTE = '/contact-requests';
 export const CONTACTS_ROUTE = '/contacts';
 export const MESSAGES_ROUTE = '/messages';
 
+export const MESSAGE_THREAD_ROUTE = '/messages/thread/:id';
+export const GET_MESSAGE_THREAD_ROUTE = id => MESSAGE_THREAD_ROUTE.replace(':id', id);
+
 export const RESET_REDUX_STORE = 'RESET_REDUX_STORE';
 
 export * from './socket-events';
