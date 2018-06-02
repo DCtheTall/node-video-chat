43a4fbf307a69098a8e05f64e8e1e3bdfcc9ae4f
Dylan Cutler <DCtheTall@users.noreply.github.com>
2018-06-02 16:15:18 -0400

    fixing sequelize

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
 const config = require('./config');
 
-const sequelize = new Sequelize(config);
+const args = process.env.NODE_ENV === 'production' ? [process.env.DATABASE_URL, config] : [config];
+const sequelize = new Sequelize(...args);
 
 sequelize.authenticate()
          .then(() => console.log('Connected to PostgreSQL database'))
