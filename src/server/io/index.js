d68e821aa43e1a8ad2ef287aab6b785b162d1e39
Dylan Cutler <DCtheTall@users.noreply.github.com>
2018-06-02 13:05:47 -0400

    fixed redis connection

--- src/server/io/index.js
+++ src/server/io/index.js
@@ -1,10 +1,14 @@
 import io from 'socket.io';
 import adapter from 'socket.io-redis';
 import { authorize } from 'socketio-jwt';
+import url from 'url';
 import { CONNECTION } from '../../constants';
 import { USER_STATUS_CHANGE } from '../schema/subscription/pubsub/constants';
 import pubsub from '../schema/subscription/pubsub';
 import attachEventHandlers from './events';
+
+const redisUrl = url.parse(process.env.REDISCLOUD_URL);
+const redisPw = redisUrl.auth.split(':')[1];
 
 /**
  * @param {Object} server HTTP server instance
@@ -12,7 +16,11 @@ import attachEventHandlers from './events';
  */
 export default function initIO(server) {
   const instance = io(server);
-  instance.adapter(adapter(process.env.REDISCLOUD_URL));
+  instance.adapter(adapter({
+    host: redisUrl.hostname,
+    port: redisUrl.port,
+    auth_pass: redisPw,
+  }));
   instance.use(authorize({
     handshake: true,
     secret: process.env.JWT_SECRET,
--- src/server/schema/subscription/pubsub/index.js
+++ src/server/schema/subscription/pubsub/index.js
@@ -1,8 +1,15 @@
 import { RedisPubSub } from 'graphql-redis-subscriptions';
+import url from 'url';
 
 export * from './constants';
 
+const redisUrl = url.parse(process.env.REDISCLOUD_URL);
+
 export default new RedisPubSub({
-  host: process.env.REDISCLOUD_URL,
+  connection: {
+    host: redisUrl.hostname,
+    port: redisUrl.port,
+    password: redisUrl.auth.split(':')[1],
+  },
   retry_strategy: options => Math.max(options.attempt * 100, 3000),
 });
